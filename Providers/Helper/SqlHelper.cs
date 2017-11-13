c29aa0b84c61216e497200118064dc3dd082cea7
Surbhi Sharma <surbhi.sharma@osvin.com>
2017-11-13 17:46:15 +0530

    sqlHelper

---
+++ Providers/Helper/SqlHelper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using Dapper;
+using System.Data;
+using MySql.Data.MySqlClient;
+
+namespace ShotPlay.Helpers
+{
+    public class SqlHelper
+    {
+        private static string constr = ConfigurationManager.ConnectionStrings["constr"].ConnectionString;
+        private MySqlConnection con = new MySqlConnection(constr);
+         
+        public int ExecuteScalar(string SpName, object param)
+        {
+            int res=Convert.ToInt32(con.ExecuteScalar(SpName, param, commandType: CommandType.StoredProcedure));
+            return res;
+        }
+        public int Execute(string SpName, object param)
+        {
+            int res = con.Execute(SpName, param, commandType: CommandType.StoredProcedure);
+            return res;
+        }
+        public IEnumerable<type> Query<type>(string SpName, object param)
+        {
+            var res = con.Query<type>(SpName, param, commandType: CommandType.StoredProcedure);
+            return res;
+        }
+    }
+}
