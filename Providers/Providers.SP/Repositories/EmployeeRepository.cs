e9c802f8e2d6f70b459c0467dc9a0a7b9646ca56
osvin <osvin315@gmail.com>
2017-11-13 15:30:24 +0530

    Naveen's first Commit

---
+++ Providers/Providers.SP/Repositories/EmployeeRepository.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DomainModel.EntityModel;
+using Providers.Repositories;
+
+namespace Providers.Providers.SP.Repositories
+{
+    public class EmployeeRepository : IEmployee, IDisposable
+    {
+        public void Dispose()
+        {
+            GC.SuppressFinalize(this);
+        }
+        public UserDetails AuthenticateUser(UserDetails model)
+        {
+            try
+            {
+                return null;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+    }
+}
