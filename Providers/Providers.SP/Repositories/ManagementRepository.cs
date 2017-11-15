b78d8fbbcb53144117ac2156c95f388c45245977
Surbhi Sharma <surbhi.sharma@osvin.com>
2017-11-15 12:11:34 +0530

    commit

---
+++ Providers/Providers.SP/Repositories/ManagementRepository.cs
@@ -0,0 +1,19 @@
+﻿using DomainModel.EntityModel;
+using Providers.Helper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Providers.Providers.SP.Repositories
+{
+    public class ManagementRepository
+    {
+        SqlHelper objHelper = new SqlHelper();
+        public List<UserDetails> UserListBySearch()
+        {
+
+        }
+    }
+}
---
+++ Providers/Repositories/IManagement.cs
@@ -0,0 +1,14 @@
+﻿using DomainModel.EntityModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Providers.Repositories
+{
+    public interface IManagement
+    {
+        List<UserDetails> UserListBySearch();
+    }
+}
