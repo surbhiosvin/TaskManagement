7a3ec4d193da38715333c860760240117d94e218
osvin <osvin315@gmail.com>
2017-11-20 12:18:16 +0530

    class

---
+++ TaskManagementOsvin/Models/AddUpdateProjectModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TaskManagementOsvin.Models
+{
+    public class AddUpdateProjectModel
+    {
+        public long ProjectId { get; set; }
+        public long ClientId { get; set; }
+        public int DepartmentId { get; set; }
+        public string TechnologyName { get; set; }
+        public int ProjectTypeId { get; set; }
+        public string ProjectTitle { get; set; }
+        public string ShortDescription { get; set; }
+        public string FullDescription { get; set; }
+        public string UploadDetailsDocument { get; set; }
+        public string StartDate { get; set; }
+        public string EndDate { get; set; }
+        public DateTime ClosedDate { get; set; }
+        public string ProjectUrl { get; set; }
+        public string ProjectStatus { get; set; }
+        public string Archived { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime UpdatedDate { get; set; }
+        public bool IsDeleted { get; set; }
+        public DateTime DeletedDate { get; set; }
+        public long CreatedBy { get; set; }
+    }
+}
